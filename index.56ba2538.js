174aee4186a96a9160e0eeea18477b7a44202eef
JordanViknar <74505993+JordanViknar@users.noreply.github.com>
2024-08-15 16:10:33 +0000

    Deploying to gh-pages from @ JordanViknar/JordanViknar.github.io@3acd7b416921ca1cc181b988be54140acc2e392b 🚀

--- index.56ba2538.js
+++
@@ -1,2 +0,0 @@
-const t=function(t){let e=document.getElementById(t);if(e)return e;throw Error(`Element with id ${t} not found`)}("taskbar-status-time");function e(){let e=new Date;e.setHours(e.getHours()+2),t.innerText=e.toLocaleTimeString("en-US",{hour:"numeric",minute:"numeric"})}setInterval(()=>{e()},1e3),e();
-//# sourceMappingURL=index.56ba2538.js.map
